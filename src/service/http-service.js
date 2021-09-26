aa4b83abb4c3266734ff41167557b26d7ca364db
KenNL42 <59517626+KenNL42@users.noreply.github.com>
2021-09-26 15:50:58 -0700

    revert index back

--- src/service/http-service.js
+++ src/service/http-service.js
@@ -3,7 +3,8 @@ import 'whatwg-fetch';
 class HttpService {
     getMovieLists = () => {
         var promise = new Promise((resolve, reject) => {  
-            fetch('http://localhost:3000/movie')
+            // fetch('http://localhost:3000/movie')
+            fetch('https://movie-shop-webapp-api.herokuapp.com/movie')
             .then(response => {
                 resolve(response.json());
             })
